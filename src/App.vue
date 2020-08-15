c370e5a39f0833f58822f712a19bd40ed42790c5
Lisa <elizabet.liber@gmail.com>
2020-08-15 16:07:20 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ todoTitle }}</h1>
+    <div class="wrap">
+<!--      <p>{{ todo }}</p>-->
+      <input type="text" v-model="todo" placeholder="type todo ...">
+      <button @click="todoAdd">add</button>
+      <ul>
+        <li v-for="(tod, index) in todos" :key="tod">
+
+          <input type="checkbox" id="checkbox" v-model="lis">
+          <p :class="{active : lis}">{{tod}}</p>
+          <button @click="todos.splice(index, 1)">delete</button>
+
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todoTitle: "Todo App",
+      todo: "",
+      todos: [],
+      lis: false
+    }
+  },
+  methods: {
+    todoAdd() {
+      if(this.todo !==""){
+      this.todos.push(this.todo)
+      console.log(this.todos)
+      this.todo=""
+    }
+    }
   }
 }
 </script>
@@ -24,5 +50,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  text-align: center;
+}
+.active{
+  text-decoration: line-through;
 }
 </style>
