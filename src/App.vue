2c005c40d88b78612a2790c37fd0fc30f3a47404
Lisa <elizabet.liber@gmail.com>
2020-08-17 03:04:15 +0700

    add styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <h1>{{ todoTitle }}</h1>
-    <div class="wrap">
-<!--      <p>{{ todo }}</p>-->
-      <input type="text" v-model="todo" placeholder="type todo ...">
-      <button @click="todoAdd">add</button>
-      <ul>
-        <li v-for="(tod, index) in todos" :key="tod">
+    <h1><em>{{ todoTitle }}</em></h1>
+    <div class="container">
 
-          <input type="checkbox" id="checkbox" v-model="lis">
-          <p :class="{active : lis}">{{tod}}</p>
-          <button @click="todos.splice(index, 1)">delete</button>
+      <div class="input-group">
+        <input type="text" class="form-control mr-1" aria-label="Text input with checkbox" v-model="todo">
+        <button @click="todoAdd" class="btn btn-success">+ add</button>
+      </div>
+      <div v-for="(tod, index) in todos" :key="tod">
 
-        </li>
-      </ul>
+        <div class="input-group mt-1">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <input v-model="lis"  type="checkbox" aria-label="Checkbox for following text input">
+              </div>
+            </div>
+
+          <input type="text" class="form-control mr-1" aria-label="Text input with checkbox" :class="{'active': lis}" :value="tod">
+          <button @click="todos.splice(index, 1)" class="btn btn-danger">delete</button>
+
+        </div>
+        </div>
+
     </div>
   </div>
 </template>
@@ -32,11 +40,11 @@ export default {
   },
   methods: {
     todoAdd() {
-      if(this.todo !==""){
-      this.todos.push(this.todo)
-      console.log(this.todos)
-      this.todo=""
-    }
+      if (this.todo !== "") {
+        this.todos.push(this.todo.trim())
+        console.log(this.todos)
+        this.todo = ""
+      }
     }
   }
 }
@@ -54,6 +62,9 @@ export default {
 
 h1 {
   text-align: center;
+}
+.container {
+  width: 600px;
 }
 .active{
   text-decoration: line-through;
